71e3bc890e9dd179024ec12431e25fb9ec46ad4e
islomnurullayev <islomnurillayev3333@gmail.com>
2022-02-03 19:06:00 +0300

    Issue #4: text styles implemented

--- lib/theme/style.dart
+++ lib/theme/style.dart
@@ -1,5 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:ibs_platform/theme/colors.dart';
 
 class Style {
+  //colors
   static AppColors colors = AppColors();
+
+  //text styles
+  static TextStyle headTitle = TextStyle(
+    color: colors.black,
+    fontSize: 36,
+    fontWeight: FontWeight.w500,
+  );
 }
