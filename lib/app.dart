7da99e2383bc447849c0bc8140c883fcce943f64
Shamsiddin Fakhriddinov <shamsiddin.fakhriddinov@s.amity.edu>
2022-02-03 18:18:31 +0500

    Issue #1: colors implemented

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:ibs_platform/theme/colors.dart';
+import 'package:ibs_platform/theme/style.dart';
 
 class IbsApp extends StatelessWidget {
   const IbsApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(color: Style.colors.primary);
   }
 }
